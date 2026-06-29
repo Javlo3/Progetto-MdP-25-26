e65851090c3b2d19f27997a97e5fd5e4aad23f6f
Ivo Zoppi <zoppiivo@gmail.com>
2026-06-29 16:48:07 +0200

    Aggiunta della classe Stregone tra i nemici

--- ProgettoMdP/src/main/java/it/unicam/cs/mpgc/rpg125585/backEnd/entita/nemici/stregoni/Stregone.java
+++ ProgettoMdP/src/main/java/it/unicam/cs/mpgc/rpg125585/backEnd/entita/nemici/stregoni/Stregone.java
@@ -1,4 +1,10 @@
-package it.unicam.cs.mpgc.rpg125585.backEnd.entita.nemici;
+package it.unicam.cs.mpgc.rpg125585.backEnd.entita.nemici.stregoni;
 
-public class Stregone {
+import it.unicam.cs.mpgc.rpg125585.backEnd.entita.nemici.Nemico;
+
+public class Stregone extends Nemico {
+
+    public Stregone() {
+        super(55, 20, 2, 3);
+    }
 }
