90f5eaccdfb2bb09e151cbdcd73e6bacacc2cf06
Ivo Zoppi <zoppiivo@gmail.com>
2026-06-30 11:40:13 +0200

    Refactoring del package InterfacciaGrafica e del Main per rendere coerente il codice

--- ProgettoMdP/src/main/java/it/unicam/cs/mpgc/rpg125585/Main.java
+++ ProgettoMdP/src/main/java/it/unicam/cs/mpgc/rpg125585/Main.java
@@ -1,11 +1,11 @@
 package it.unicam.cs.mpgc.rpg125585;
 
 import javafx.application.Application;
-import it.unicam.cs.mpgc.rpg125585.GUI.GUI;
+import it.unicam.cs.mpgc.rpg125585.gui.InterfacciaGrafica;
 
 public class Main {
     static void main(String[] args) {
         System.out.println("Avvio del sistema...");
-        Application.launch(GUI.class, args);
+        Application.launch(InterfacciaGrafica.class, args);
     }
 }
--- ProgettoMdP/src/main/java/it/unicam/cs/mpgc/rpg125585/gui/InterfacciaGrafica.java
+++ ProgettoMdP/src/main/java/it/unicam/cs/mpgc/rpg125585/gui/InterfacciaGrafica.java
@@ -1,9 +1,9 @@
-package it.unicam.cs.mpgc.rpg125585.GUI;
+package it.unicam.cs.mpgc.rpg125585.gui;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class GUI extends Application {
+public class InterfacciaGrafica extends Application {
 
     @Override
     public void start(Stage stage) {
