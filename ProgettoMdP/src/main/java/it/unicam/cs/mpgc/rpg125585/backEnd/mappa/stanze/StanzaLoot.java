183e30e7a301177584348495729f7709ed1f4c0e
Ivo Zoppi <zoppiivo@gmail.com>
2026-06-30 10:27:53 +0200

    Implementazione iniziale della classe StanzaLoot

--- ProgettoMdP/src/main/java/it/unicam/cs/mpgc/rpg125585/backEnd/mappa/stanze/StanzaLoot.java
+++ ProgettoMdP/src/main/java/it/unicam/cs/mpgc/rpg125585/backEnd/mappa/stanze/StanzaLoot.java
@@ -1,8 +1,20 @@
 package it.unicam.cs.mpgc.rpg125585.backEnd.mappa.stanze;
 
-public class StanzaLoot extends StanzaGenerica {
+import it.unicam.cs.mpgc.rpg125585.backEnd.artefatti.Artefatto;
 
-    public StanzaLoot(String nome, String descrizione) {
+public class StanzaLoot extends StanzaGenerica {
+    private Artefatto artefatto;
+
+    public StanzaLoot(String nome, String descrizione, Artefatto artefatto) {
         super(nome, descrizione);
+        this.artefatto = artefatto;
+    }
+
+    public Artefatto getArtefatto() {
+        return artefatto;
+    }
+
+    public void setArtefatto(Artefatto artefatto) {
+        this.artefatto = artefatto;
     }
 }
