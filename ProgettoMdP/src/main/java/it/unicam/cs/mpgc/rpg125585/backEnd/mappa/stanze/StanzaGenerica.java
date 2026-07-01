69ec1d56676c7fba37b789ceb4ae20760ade7029
Ivo Zoppi <zoppiivo@gmail.com>
2026-07-01 15:41:12 +0200

    Aggiunta degli attributi e relativi Getter e Setter per la gestione delle stanze adiacenti

--- ProgettoMdP/src/main/java/it/unicam/cs/mpgc/rpg125585/backEnd/mappa/stanze/StanzaGenerica.java
+++ ProgettoMdP/src/main/java/it/unicam/cs/mpgc/rpg125585/backEnd/mappa/stanze/StanzaGenerica.java
@@ -1,11 +1,55 @@
 package it.unicam.cs.mpgc.rpg125585.backEnd.mappa.stanze;
 
 public abstract class StanzaGenerica {
-    private String nome;
-    private String descrizione;
+    private final String nome;
+    private final String descrizione;
+    private StanzaGenerica stanzaNord;
+    private StanzaGenerica stanzaSud;
+    private StanzaGenerica stanzaEst;
+    private StanzaGenerica stanzaOvest;
 
     public StanzaGenerica(String nome, String descrizione) {
         this.nome = nome;
         this.descrizione = descrizione;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public StanzaGenerica getStanzaNord() {
+        return stanzaNord;
+    }
+
+    public void setStanzaNord(StanzaGenerica stanzaNord) {
+        this.stanzaNord = stanzaNord;
+    }
+
+    public StanzaGenerica getStanzaSud() {
+        return stanzaSud;
+    }
+
+    public void setStanzaSud(StanzaGenerica stanzaSud) {
+        this.stanzaSud = stanzaSud;
+    }
+
+    public StanzaGenerica getStanzaEst() {
+        return stanzaEst;
+    }
+
+    public void setStanzaEst(StanzaGenerica stanzaEst) {
+        this.stanzaEst = stanzaEst;
+    }
+
+    public StanzaGenerica getStanzaOvest() {
+        return stanzaOvest;
+    }
+
+    public void setStanzaOvest(StanzaGenerica stanzaOvest) {
+        this.stanzaOvest = stanzaOvest;
     }
 }
